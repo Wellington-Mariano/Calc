996bb07477ea780bee9666dc48ff8b9c8da59939
Wellington Mariano <wellington.mariano97@hotmail.com>
2023-02-28 10:09:50 +0000

    Created func to import values from HTML el
    In this change i removed the get element by id from all the event listeners functions to clean the code a bit more.

--- index.js
+++ index.js
@@ -6,49 +6,54 @@ const sub_button = document.getElementById("subtraction--btn");
 const mul_button = document.getElementById("multiplication--btn");
 const div_button = document.getElementById("division--btn");
 //OBJECT CONTAINING ALL OPERATIONS IN THIS CALCULATOR
+
 const calc_obj = {
   operations: {
     addition: function (input_one, input_two) {
-      return result = input_one + input_two;
+      return input_one + input_two;
     },
     subtraction: function (input_one, input_two) {
       return input_one - input_two;
     },
     multiplication: function (input_one, input_two) {
-        return input_one * input_two;
+      return input_one * input_two;
     },
     division: function (input_one, input_two) {
-        return input_one / input_two;
-    }
-  }
+      return input_one / input_two;
+    },
+  },
 };
 
-let result = 0;// initiazling var to receive all result
+let result = 0; // initiazling var to receive all
 
-sum_button.addEventListener("click", function () {
+function getVals() {
   const num = Number(document.getElementById("number_input--1").value);
   const num2 = Number(document.getElementById("number_input--2").value);
+  return num, num2; // assigne it to the other functions so the values are updated
+}
+
+//i could also just use a event click function and pass the operation as a parameter
+
+sum_button.addEventListener("click", function () {
+  getVals();
   result = calc_obj.operations.addition(num, num2);
   result_el.textContent = result;
 });
 
 sub_button.addEventListener("click", function () {
-  const num = Number(document.getElementById("number_input--1").value);
-  const num2 = Number(document.getElementById("number_input--2").value);
+  getVals();
   result = calc_obj.operations.subtraction(num, num2);
   result_el.textContent = result;
 });
 
 mul_button.addEventListener("click", function () {
-  const num = Number(document.getElementById("number_input--1").value);
-  const num2 = Number(document.getElementById("number_input--2").value);
+  getVals();
   result = calc_obj.operations.multiplication(num, num2);
   result_el.textContent = result;
 });
 
 div_button.addEventListener("click", function () {
-  const num = Number(document.getElementById("number_input--1").value);
-  const num2 = Number(document.getElementById("number_input--2").value);
+  getVals();
   result = calc_obj.operations.division(num, num2);
   result_el.textContent = result;
 });
