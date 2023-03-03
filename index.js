97ebfd2cc93b2208e07a661ddb33fa882126b267
Wellington Mariano <wellington.mariano97@hotmail.com>
2023-03-03 10:22:52 +0000

    final version
    final result

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 "use strict";
-
 const result_el = document.getElementById("result");
-const sum_button = document.querySelector(".addition");
-const sub_button = document.getElementById("subtraction--btn");
-const mul_button = document.getElementById("multiplication--btn");
-const div_button = document.getElementById("division--btn");
 //OBJECT CONTAINING ALL OPERATIONS IN THIS CALCULATOR
 
 const calc_obj = {
@@ -26,34 +21,37 @@ const calc_obj = {
 
 let result = 0; // initiazling var to receive all
 
+let num1par, num2par;
+
 function getVals() {
-  const num = Number(document.getElementById("number_input--1").value);
-  const num2 = Number(document.getElementById("number_input--2").value);
-  return num, num2; // assigne it to the other functions so the values are updated
+  const num = document.getElementById("number_input--1").value;
+  num1par = parseInt(num);
+  const num2 = document.getElementById("number_input--2").value;
+  num2par = parseInt(num);
+  return num1par, num2par; // assign  it to the other functions so the values are updated
 }
 
-//i could also just use a event click function and pass the operation as a parameter
-
-sum_button.addEventListener("click", function () {
+function addition() {
   getVals();
-  result = calc_obj.operations.addition(num, num2);
+  result = calc_obj.operations.addition(num1par, num2par  );
   result_el.textContent = result;
-});
+  
+}
 
-sub_button.addEventListener("click", function () {
+function subtraction() {
   getVals();
-  result = calc_obj.operations.subtraction(num, num2);
+  result = calc_obj.operations.subtraction(num1par, num2par);
   result_el.textContent = result;
-});
+}
 
-mul_button.addEventListener("click", function () {
+function multiplication() {
   getVals();
-  result = calc_obj.operations.multiplication(num, num2);
+  result = calc_obj.operations.multiplication(num1par, num2par);
   result_el.textContent = result;
-});
+}
+function division() {
+  getVals();
 
-div_button.addEventListener("click", function () {
-  getVals();
-  result = calc_obj.operations.division(num, num2);
+  result = calc_obj.operations.division(num1par, num2par);
   result_el.textContent = result;
-});
+}
